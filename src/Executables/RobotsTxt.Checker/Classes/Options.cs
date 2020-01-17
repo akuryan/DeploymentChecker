8b20be2a68074eb693f920066993b7af49866ad8
Anton Kuryan <Anton_Kuryan@epam.com>
2020-01-17 14:57:07 +0300

    Started working on #8

--- src/Executables/RobotsTxt.Checker/Classes/Options.cs
+++ src/Executables/RobotsTxt.Checker/Classes/Options.cs
@@ -8,6 +8,9 @@ namespace RobotsTxt.Checker.Classes
         [Option(Required = true, HelpText = "Please, provide comma or space -separated list of web app root URLs, where robots.txt shall be checked. If protocol (http or https) is not provided - I am assuming it is https. Example: https://www.google.com/, https://www.gmail.com/", Separator = ',')]
         public IEnumerable<string> WebAppUrls { get; set; }
 
+        [Option(Required = false, HelpText = "If specified, request for robots.txt will be sent to this host and hostnames from --webappurls will be sent in Host header. This is useful, when verifying staging slot of Azure web app for correct robots.txt, before switching slots. (curl example: curl https://www.test.com/robots.txt --header 'Host: somehost.to')")]
+        public string ServerHostName { get; set; }
+
         [Option(Default = false, HelpText = "Define, if robots.txt shall deny all crawling of web app. Default value is false, e.g. crawling shall be allowed.")]
         public bool CrawlingDenied { get; set; }
     }
