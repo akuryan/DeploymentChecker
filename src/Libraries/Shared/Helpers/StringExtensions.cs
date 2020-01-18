eb80587d8a12a3579b1a6024dd73d89f97afa966
Anton Kuryan <Anton_Kuryan@epam.com>
2020-01-18 10:18:56 +0300

    Added string extension and red tests for it

---
+++ src/Libraries/Shared/Helpers/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Helpers
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Parses supplied string and extracts host header from it
+        /// </summary>
+        /// <param name="hostname"></param>
+        /// <returns></returns>
+        public static string CreateValidHostHeader(this string hostname)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Libraries/SharedTests/Helpers/StringExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Shared.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedTests.Helpers
+{
+    public class StringExtensionsTests
+    {
+        [TestCase("dobryak.org", "dobryak.org")]
+        [TestCase("http://dobryak.org", "dobryak.org")]
+        [TestCase("https://dobryak.org", "dobryak.org")]
+        [TestCase("://dobryak.org", "dobryak.org")]
+        [TestCase("https://dobryak.org/u/", "dobryak.org")]
+        [TestCase("https://dobryak.org/u/?tset", "dobryak.org")]
+        [TestCase("https://dobrya?k.org/u/", null)]
+        [Test, Category("StringExtensions")]
+        public void CreateValidHostHeaderTests(string suppliedData, string expectedHeader)
+        {
+            var header = suppliedData.CreateValidHostHeader();
+            Assert.AreEqual(header, expectedHeader);
+        }
+    }
+}
